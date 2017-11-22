fa4dfc86d6eddee2067e95354455c75e2ba4373d
Samuel Abudu <blvckleumas@gmail.com>
2017-11-22 09:59:13 +0100

    feature: modify center route

--- server/routes/center.js
+++ server/routes/center.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/', validate.addCenter, centerController.add);
 router.get('/', centerController.getAll);
 router.get('/:centerId', validate.centerId, centerController.retrieve);
+router.put('/:centerId', validate.centerId, validate.addCenter, centerController.modify);
+
 
 export default router;
