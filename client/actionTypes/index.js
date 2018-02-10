83fe674058d2552bb5ec3cfb47e4c968e1765056
Samuel <abudusamuelbiodun@gmail.com>
2018-02-10 16:20:22 +0100

    Modify: refactored landing page components to display limited events

--- client/actionTypes/index.js
+++ client/actionTypes/index.js
@@ -1,8 +1,9 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_UP = 'SIGN_UP';
 export const GET_ALL_CENTERS = 'GET_ALL_CENTERS';
-export const GET_TRENDING_CENTERS = 'GET_ALL_CENTERS';
+export const GET_TRENDING_CENTERS = 'GET_TRENDING_CENTERS';
 export const ADD_CENTER = 'ADD_CENTER';
 export const GET_ALL_EVENTS = 'GET_ALL_EVENTS';
+export const GET_POPULAR_EVENTS = 'GET_POPULAR_EVENTS';
 export const GET_SINGLE_EVENT = 'GET_SINGLE_EVENT';
 
